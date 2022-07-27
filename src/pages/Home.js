09e8a03ea125fd5579f4f0cb2a4f85cc70265947
tborba1 <taryn.borba@gmail.com>
2022-07-26 20:34:23 -0700

    added isLoading to Home and Products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,16 +9,18 @@ import { Outlet } from "react-router-dom";
 export default function Categories () {
 
     const [categories, setCategories] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     console.log(categories);
 
     useEffect(() => {
+        setIsLoading(true);
         const getCategories = async() => {
-            const url = `https://fakestoreapi.com/products/categories`;
-            const response = await fetch(url);
-            const data = await response.json();
-            // console.log(data);
-            setCategories(data);
-            
+        const url = `https://fakestoreapi.com/products/categories`;
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data);
+        setCategories(data);
+        setIsLoading(false);    
         }
         getCategories();
     },[]);
@@ -26,20 +28,27 @@ export default function Categories () {
     return (
         <>
             <SubTitle>Categories</SubTitle>
-            <CategoryContainer>
-            {categories.map((category) => {
-                return (
-                    <>
-                        <CategoryBox>
-                            <div className="content">
-                                <h3>{category}</h3>
-                            </div>
-                        </CategoryBox>
-                    </>
-                )
-            })}
+            {!isLoading ? (
+                <CategoryContainer>
+                    {categories?.length > 0 ? (
+                        categories?.map((category) => (
+                            <>
+                                <CategoryBox>
+                                    <div className="content">
+                                        <h3>{category}</h3>
+                                    </div>
+                                </CategoryBox>
+                            </>
+                ))
+            ): (
+                <div>No results found.</div>
+            )}
             </CategoryContainer>
+            ): (
+                <div style={{textAlign: "center"}}>Loading...</div>
+            )}
             <Outlet />
         </>
     );
 }
+
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -13,16 +13,18 @@ import { Outlet } from "react-router-dom";
 export default function Products () {
 
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     console.log(products);
 
     useEffect(() => {
+        setIsLoading(true);
         const getProducts = async() => {
-            const url = `https://fakestoreapi.com/products`;
-            const response = await fetch(url);
-            const data = await response.json();
-            // console.log(data);
-            setProducts(data);
-            
+        const url = `https://fakestoreapi.com/products`;
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data);
+        setProducts(data);
+        setIsLoading(false);
         }
         getProducts();
     },[]);
@@ -30,21 +32,27 @@ export default function Products () {
     return (
         <>
             <SubTitle>Products</SubTitle>
-            <ProductContainer>
-            {products.map((product) => {
-                return (
-                    <>
-                        <ProductBox>
-                            <div className="content">
-                                <ProductImage src={product.image} />
-                                <p>${product.price}</p>
-                                <h5>{product.title}</h5>
-                            </div>
-                        </ProductBox>
-                    </>
-                )
-            })}
+            {!isLoading ? (
+                <ProductContainer>
+                    {products?.length > 0 ? (
+                        products?.map((product) => (
+                            <>
+                                <ProductBox>
+                                    <div className="content">
+                                        <ProductImage src={product.image} />
+                                        <p>${product.price}</p>
+                                        <h5>{product.title}</h5>
+                                    </div>
+                                </ProductBox>
+                            </>
+                ))
+            ): (
+                <div>No results found.</div>
+            )}
             </ProductContainer>
+            ): (
+                <div style={{textAlign: "center"}}>Loading...</div>
+            )}
             <Outlet />
         </>
     );
