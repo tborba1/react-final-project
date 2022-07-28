1886bf34486eb58805a24bb9b47f906011486481
tborba1 <taryn.borba@gmail.com>
2022-07-28 16:47:54 -0700

    added Category filters on Products page

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -3,120 +3,129 @@ import styled from "styled-components";
 // HeaderFooter
 
 export const Title = styled.h1`
-    background-color: lightgrey;
-    font-style: italic;
-    text-align: center;
-    margin-top: 20px;
-    // margin-left: 32px;
-    // margin-right: 32px;
-    // border-radius: 10px;
+  background-color: lightgrey;
+  font-style: italic;
+  text-align: center;
+  margin-top: 20px;
+  // margin-left: 32px;
+  // margin-right: 32px;
+  // border-radius: 10px;
 `;
 export const SubTitle = styled.h2`
-    background-color: lightgrey;
-    font-style: italic;
-    text-align: center;
-    margin-top: 20px;
-    // margin-left: 32px;
-    // margin-right: 32px;
-    // border-radius: 10px;
+  background-color: lightgrey;
+  font-style: italic;
+  text-align: center;
+  margin-top: 20px;
+  // margin-left: 32px;
+  // margin-right: 32px;
+  // border-radius: 10px;
 `;
 
 export const NavBar = styled.nav`
-    background-color: #005E54;
-    display: flex;
-    justify-content: center;
+  background-color: #005e54;
+  display: flex;
+  justify-content: center;
 `;
 
 export const HeaderLink = styled.div`
-    margin: 18px 24px;
-    // color: white;
+  margin: 18px 24px;
+  // color: white;
 `;
 
 export const Footer = styled.footer`
-    background-color: #003547;
-    color: white;
-    text-align: center;
-    font-weight: bold;
+  background-color: #003547;
+  color: white;
+  text-align: center;
+  font-weight: bold;
 `;
 
 // Home for Categories
 
 export const CategoryContainer = styled.div`
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    text-align: center;
+  display: flex;
+  justify-content: center;
 `;
-  
+
 export const CategoryBox = styled.div`
-    border: 2px solid black;
-    width: 10%;
-    margin: 10px;
-    text-align: center;
+  border: 2px solid black;
+  width: 10%;
+  margin: 10px;
+  text-align: center;
+`;
+
+// Filters
+
+export const FilterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const FilterButton = styled.button`
+  margin: 10px;
+  font-family: Nunito;
+  background-color: #ed8b16;
 `;
 
 // Products
 
 export const ProductContainer = styled.div`
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    text-align: center;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  text-align: center;
 `;
-  
+
 export const ProductBox = styled.div`
-    border: 2px solid black;
-    width: 20%;
-    margin: 10px;
-    padding: 10px;
-    text-align: center;
+  border: 2px solid black;
+  width: 20%;
+  margin: 10px;
+  padding: 10px;
+  text-align: center;
 `;
 
 export const ProductDetailsBox = styled.div`
-    border: 2px solid black;
-    width: 50%;
-    margin: 10px;
-    padding: 10px;
-    text-align: center;
+  border: 2px solid black;
+  width: 50%;
+  margin: 10px;
+  padding: 10px;
+  text-align: center;
 `;
 
 export const ProductContent = styled.div`
-    background-color: #B7BF99;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  background-color: #b7bf99;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const ProductImage = styled.img`
-    width: 50%;
-    height: 100%;
+  width: 50%;
+  height: 100%;
 `;
 
 export const DetailsButton = styled.button`
-    font-family: Nunito;
-    background-color: #C2BB00;
+  font-family: Nunito;
+  background-color: #c2bb00;
 `;
 
 // Login Form
 
 export const FormContainer = styled.div`
-    background-color: #B7BF99;
-    // width: 50%;
-    // height: 50%;
-    margin-left: 200px;
-    margin-right: 200px;
-    padding: 50px;
-    border: 2px solid black;  
+  background-color: #b7bf99;
+  // width: 50%;
+  // height: 50%;
+  margin-left: 200px;
+  margin-right: 200px;
+  padding: 50px;
+  border: 2px solid black;
 `;
 
 export const LoginButton = styled.button`
-    font-family: Nunito;
-    background-color: #005E54;
-    color: white;
+  font-family: Nunito;
+  background-color: #005e54;
+  color: white;
 `;
-
 
 /* Color Theme Swatches in Hex 
 color: #003547; dark blue
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,58 +1,97 @@
 // Display the image, title, and price of all products in a list or grid.
 // When a product is clicked, navigate to the detail page for the selected product.
 
-import { useState, useEffect } from "react";
-import { 
-    SubTitle, 
-    ProductContainer,
-    ProductBox,
+import React, { useState, useEffect } from "react";
+import {
+  SubTitle,
+  ProductContainer,
+  ProductBox,
+  FilterButton,
+  FilterContainer,
 } from "../components/StyledComponents";
 import ProductCard from "../components/ProductCard";
 import { Outlet } from "react-router-dom";
 
-export default function Products (props) {
+export default function Products(props) {
+  const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState(products);
+  const [isLoading, setIsLoading] = useState(false);
+  let componentMounted = true;
+  // console.log(products);
 
-    const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    console.log(products);
-
-    useEffect(() => {
-        setIsLoading(true);
-        const getProducts = async() => {
-        const url = `https://fakestoreapi.com/products`;
-        const response = await fetch(url);
-        const data = await response.json();
-        // console.log(data);
-        setProducts(data);
+  useEffect(() => {
+    setIsLoading(true);
+    const getProducts = async () => {
+      const url = `https://fakestoreapi.com/products`;
+      const response = await fetch(url);
+      if (componentMounted) {
+        setProducts(await response.clone().json());
+        setFilter(await response.json());
         setIsLoading(false);
-        }
-        getProducts();
-    },[]);
+        console.log(filter);
+      }
 
-    return (
-        <>
-        <SubTitle>Products</SubTitle>    
-            {!isLoading ? (
-                <ProductContainer>
-                    {products?.length > 0 ? (
-                        products?.map((product) => (
-                            <ProductBox>
-                                <ProductCard
-                                title={product.title}
-                                price={product.price}
-                                image={product.image}
-                                id={product.id} />
-                            </ProductBox>
-                ))
-            ): (
-                <div>No results found.</div>
-            )}
-            </ProductContainer>
-            ): (
-                <div style={{textAlign: "center"}}>Loading...</div>
-            )}
-            <Outlet />
-        </>
-    );
+      return () => {
+        componentMounted = false;
+      };
+    };
+    getProducts();
+  }, []);
+
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     // console.log(data);
+  //     setProducts(data);
+  //     setIsLoading(false);
+  //     }
+  //     getProducts();
+  // },[]);
+
+  const filterProduct = (cat) => {
+    const updatedList = products.filter((x) => x.category === cat);
+    setFilter(updatedList);
+  };
+
+  return (
+    <>
+      <SubTitle>Products</SubTitle>
+      <FilterContainer>
+        <FilterButton onClick={() => setFilter(products)}>All</FilterButton>
+        <FilterButton onClick={() => filterProduct("electronics")}>
+          Electronics
+        </FilterButton>
+        <FilterButton onClick={() => filterProduct("jewelery")}>
+          Jewelery
+        </FilterButton>
+        <FilterButton onClick={() => filterProduct("men's clothing")}>
+          Men's Clothing
+        </FilterButton>
+        <FilterButton onClick={() => filterProduct("women's clothing")}>
+          Women's Clothing
+        </FilterButton>
+      </FilterContainer>
+
+      {!isLoading ? (
+        <ProductContainer>
+          {products?.length > 0 ? (
+            filter.map((product) => (
+              <ProductBox>
+                <ProductCard
+                  title={product.title}
+                  price={product.price}
+                  image={product.image}
+                  id={product.id}
+                />
+              </ProductBox>
+            ))
+          ) : (
+            <div>No results found.</div>
+          )}
+        </ProductContainer>
+      ) : (
+        <div style={{ textAlign: "center" }}>Loading...</div>
+      )}
+      <Outlet />
+    </>
+  );
 }
