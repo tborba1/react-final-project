59e522bd231a722a543f91e8beed44634961867c
tborba1 <taryn.borba@gmail.com>
2022-07-27 19:33:37 -0700

    added basic form to Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 export default function Login () {
+    
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Form Submitted");
+    }
+    
     return (
         <>
             <p style={{textAlign: "center"}}>Login Page</p>
+            <form onSubmit={handleSubmit}>
+                <label for="Email">Email</label>
+                <input name="Email" value={email} setValue={setEmail} />
+                <label for="Password">Password</label>
+                <input name="Password" label="Password" value={password} setValue={setPassword} inputType="password" />
+
+            </form>
         </>
     );
 }
