b89aea8514e45ea9c4bc9dc781a7a365c0c7c327
tborba1 <taryn.borba@gmail.com>
2022-08-03 19:45:24 -0700

    added AddButton and link on ProductDetails page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -5,34 +5,36 @@
 
 import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import NavLink from "../components/NavLink";
 import {
   SubTitle,
   ProductDetailsBox,
   ProductImage,
   ProductContent,
+  AddButton,
 } from "../components/StyledComponents";
-// import { CartContext } from "../contexts/cart/CartContext";
+import { CartContext } from "../contexts/cart/CartContext";
 
 export default function ProductDetails() {
   const { id } = useParams();
   const [details, setDetails] = useState([]);
-  // const { cart, setCart } = useContext(CartContext);
-  // const [isLoading, setIsLoading] = useState(false);
+  const { cart, setCart } = useContext(CartContext);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // const addToCart = (details) => {
-  //   setCart(details);
-  //   console.log(cart);
-  // };
+  const addToCart = (details) => {
+    setCart(details);
+    console.log(cart);
+  };
 
   useEffect(() => {
-    // setIsLoading(true);
+    setIsLoading(true);
     const getDetails = async () => {
       const url = `https://fakestoreapi.com/products/${id}`;
       const response = await fetch(url);
       const data = await response.json();
       // console.log(data);
       setDetails(data);
-      // setIsLoading(false);
+      setIsLoading(false);
     };
     getDetails();
   }, [id]);
@@ -40,6 +42,7 @@ export default function ProductDetails() {
   return (
     <>
       <SubTitle>Product Details</SubTitle>
+      <NavLink route="/products" label="Back to Products" />
       <ProductDetailsBox>
         <ProductImage src={details.image} />
         <ProductContent>
@@ -50,9 +53,9 @@ export default function ProductDetails() {
           <p style={{ fontWeight: "bold" }}>Description:</p>
           <p>{details.description}</p>
         </ProductContent>
-        {/* <button onClick={() => addToCart([...cart, details])}>
+        <AddButton onClick={() => addToCart([...cart, details])}>
           Add to Cart
-        </button> */}
+        </AddButton>
       </ProductDetailsBox>
     </>
   );
